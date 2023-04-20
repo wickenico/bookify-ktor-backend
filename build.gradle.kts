50980a4e957392396ef3c633e247026a6781a7c5
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2023-04-20 04:18:23 +0200

    feat: Automate gradle version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,16 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val exposed_version: String by project
-val swagger_codegen_version: String by project
-val appVersion: String by project
-
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     application
-    kotlin("jvm") version "1.8.20"
-    id("io.ktor.plugin") version "2.2.4"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20"
-    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.ktorPlugin)
+    alias(libs.plugins.versionUpdate)
+    alias(libs.plugins.catalogUpdate)
 }
 
 group = "com.nw"
-version = "$appVersion"
+version = "0.2.0"
 application {
     mainClass.set("io.ktor.server.cio.EngineMain")
 
@@ -28,54 +24,16 @@ repositories {
 }
 
 dependencies {
-    // Ktor Server
-    implementation("io.ktor:ktor-server-default-headers-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-forwarded-header-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
-    implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-cio-jvm:$ktor_version")
-
-    // Ktor Client
-    implementation("io.ktor:ktor-client-cio-jvm:$ktor_version")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
-
-    // Serialization
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-jackson-jvm:$ktor_version")
-
-    // Exposed SQL
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-
-    // DB Connector
-    implementation("mysql:mysql-connector-java:8.0.32")
-
-    // Logging
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-
-    // Swagger and OpenAPI
-    implementation("io.ktor:ktor-server-openapi:$ktor_version")
-    implementation("io.swagger.codegen.v3:swagger-codegen-generators:$swagger_codegen_version")
-    implementation("io.ktor:ktor-server-swagger:$ktor_version")
-
-    // Security
-    implementation("io.ktor:ktor-server-auth:$ktor_version")
-    implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
-
-    // Cookies
-    implementation("io.ktor:ktor-server-sessions:$ktor_version")
-
-    implementation("org.testng:testng:7.7.0")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm:2.2.3")
-
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation(libs.bundles.ktorServer)
+    implementation(libs.bundles.ktorClient)
+    implementation(libs.bundles.ktorSerialization)
+    implementation(libs.bundles.exposed)
+    implementation(libs.bundles.dbConnector)
+    implementation(libs.bundles.logging)
+    implementation(libs.bundles.swaggerAndOpenAPI)
+    implementation(libs.bundles.security)
+    implementation(libs.bundles.cookies)
+    implementation(libs.bundles.testing)
 }
 
 ktor {
