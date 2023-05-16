3788f9d8520825edcec9443789f515b0c244fe10
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2023-05-16 21:35:12 +0200

    Add new status pages

--- src/main/kotlin/com/nw/plugins/StatusPages.kt
+++ src/main/kotlin/com/nw/plugins/StatusPages.kt
@@ -8,14 +8,38 @@ import io.ktor.server.response.respondText
 
 fun Application.configureStatusPages() {
     install(StatusPages) {
-        status(HttpStatusCode.MethodNotAllowed) { call, status ->
-            call.respondText(text = "405: Method Not Allowed", status = status)
+        status(HttpStatusCode.BadRequest) { call, status ->
+            call.respondText(text = "400: Bad Request", status = status)
+        }
+        status(HttpStatusCode.Unauthorized) { call, status ->
+            call.respondText(text = "401: Unauthorized request.", status = status)
+        }
+        status(HttpStatusCode.Forbidden) { call, status ->
+            call.respondText(text = "403: Forbidden", status = status)
         }
         status(HttpStatusCode.NotFound) { call, status ->
             call.respondText(text = "404: Not Found", status = status)
         }
-        status(HttpStatusCode.Unauthorized) { call, status ->
-            call.respondText(text = "401: Unauthorized request.", status = status)
+        status(HttpStatusCode.MethodNotAllowed) { call, status ->
+            call.respondText(text = "405: Method Not Allowed", status = status)
+        }
+        status(HttpStatusCode.Conflict) { call, status ->
+            call.respondText(text = "409: Conflict", status = status)
+        }
+        status(HttpStatusCode.UnprocessableEntity) { call, status ->
+            call.respondText(text = "422: Unprocessable Entity", status = status)
+        }
+        status(HttpStatusCode.TooManyRequests) { call, status ->
+            call.respondText(text = "429: Too Many Requests", status = status)
+        }
+        status(HttpStatusCode.InternalServerError) { call, status ->
+            call.respondText(text = "500: Internal Server Error", status = status)
+        }
+        status(HttpStatusCode.ServiceUnavailable) { call, status ->
+            call.respondText(text = "503: Service Unavailable", status = status)
+        }
+        status(HttpStatusCode.GatewayTimeout) { call, status ->
+            call.respondText(text = "504: Gateway Timeout", status = status)
         }
     }
 }
